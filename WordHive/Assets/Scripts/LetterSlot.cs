8e8011f395ad1f7c4dbbdcb6dc13c0ec345068c2
Eduardo <eduardoarcunha12@gmail.com>
2023-11-02 14:29:47 -0300

    feat: started game grid

---
+++ WordHive/Assets/Scripts/LetterSlot.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LetterSlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount == 0)
+        {
+            GameObject dropped = eventData.pointerDrag;
+            DraggableLetter draggableLetter = dropped.GetComponent<DraggableLetter>();
+            draggableLetter.parentAfterDrag = transform;
+        }
+        else
+        {
+            GameObject dropped = eventData.pointerDrag;
+            DraggableLetter draggableLetter = dropped.GetComponent<DraggableLetter>();
+
+            GameObject current = transform.GetChild(0).gameObject;
+            DraggableLetter currentDraggable = current.GetComponent<DraggableLetter>();
+
+            currentDraggable.transform.SetParent(draggableLetter.parentAfterDrag);
+            draggableLetter.parentAfterDrag = transform;
+        }
+    }
+}
